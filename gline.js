c3ac04b06f049db38405837653b6495dbab8b403
HamzaAesmael <hesmael27@gmail.com>
2025-05-19 08:32:40 +0300

    init commit

---
+++ gline.js
@@ -0,0 +1,3 @@
+gline = [ ['Month','umich.edu','cam.ac.uk','virginia.edu','unicon.net','mac.com','rsmart.com','mtu.edu','columbia.edu','gmail.com','northwestern.edu'],
+['2005-12',6,4,4,4,2,2,2,2,2,2]
+];
